ca4af45f9d69e74c107758deadbe676aab8ae3c9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2011-03-25 04:21:43 +0900

    Run tests with explicit ./ld-mac

--- runtests.sh
+++ runtests.sh
@@ -11,11 +11,11 @@ MAC_TOOL_DIR=/usr/i686-apple-darwin10
 MAC_BIN_DIR=$MAC_TOOL_DIR/usr/bin
 
 echo "Running gcc -c mach/hello.c"
-$MAC_BIN_DIR/gcc -c mach/hello.c
+./ld-mac $MAC_BIN_DIR/gcc -c mach/hello.c
 echo "Running gcc mach/hello.c"
-$MAC_BIN_DIR/gcc mach/hello.c
+./ld-mac $MAC_BIN_DIR/gcc mach/hello.c
 echo "Running gcc -g mach/hello.c"
-PATH=$MAC_BIN_DIR $MAC_BIN_DIR/gcc -g mach/hello.c
+PATH=$MAC_BIN_DIR ./ld-mac $MAC_BIN_DIR/gcc -g mach/hello.c
 
 echo
 echo '*** ALL TESTS PASS ***'
