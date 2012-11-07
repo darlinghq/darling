35d806ce923bc1533029383e8f93ff9ca7f33b55
Lubos Dolezel <lubos@dolezel.info>
2012-11-07 21:35:49 +0100

    Attempt to fix boostxml

--- tests/boostxml.cpp
+++ tests/boostxml.cpp
@@ -5,6 +5,7 @@ boostxml::boostxml()
 	m_xml << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	m_xml << "<TestLog>\n";
 	m_xml << "\t<TestSuite name=\"Darling\">\n";
+	m_xml << "\t<TestSuite name=\"tests\">\n";
 }
 
 void boostxml::addOK(const std::string& path, int time)
@@ -26,7 +27,7 @@ void boostxml::addFailure(const std::string& path, int time, const std::string& 
 
 std::string boostxml::str()
 {
-	m_xml << "\t</TestSuite>\n</TestLog>\n";
+	m_xml << "\t</TestSuite></TestSuite>\n</TestLog>\n";
 	return m_xml.str();
 }
 
