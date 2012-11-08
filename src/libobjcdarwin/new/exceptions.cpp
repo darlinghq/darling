51c3870fe050c8239558b266dfc220bcf33b5686
Lubos Dolezel <lubos@dolezel.info>
2012-11-09 00:08:47 +0100

    Fix ObjC exceptions on ABI 2

--- src/libobjcdarwin/new/exceptions.cpp
+++ src/libobjcdarwin/new/exceptions.cpp
@@ -5,6 +5,8 @@ extern "C" void __cxa_rethrow();
 extern "C" void* __cxa_begin_catch(void*);
 extern "C" void __cxa_end_catch();
 
+static __thread bool m_cxx = false;
+
 void objc_exception_rethrow()
 {
 	__cxa_rethrow();
@@ -15,13 +17,20 @@ void* objc_begin_catch(void* p)
 	void *rv = returnReturn();
 	void* cpp = __cxa_begin_catch(p);
 	if (cpp)
+	{
+		m_cxx = true;
 		return cpp;
+	}
 	else
+	{
+		m_cxx = false;
 		return rv;
+	}
 }
 
 void objc_end_catch()
 {
-	__cxa_end_catch();
+	if (m_cxx)
+		__cxa_end_catch();
 }
 
