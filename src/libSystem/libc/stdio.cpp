0d360154958d0be75b5d1fb3958cd1a8f154e565
Lubos Dolezel <lubos@dolezel.info>
2012-12-09 20:06:51 +0100

    fstream sFILE/IO_FILE workaround not doable on x86, omitting

--- src/libSystem/libc/stdio.cpp
+++ src/libSystem/libc/stdio.cpp
@@ -405,9 +405,17 @@ std::__basic_file<char>* _ZNSt12__basic_fileIcE8sys_openEP7__sFILESt13_Ios_Openm
 	return pThis->sys_open(f->linux_fp, mode);
 }
 
+#ifdef __x86_64__
+
 extern "C" void _ZN9__gnu_cxx13stdio_filebufIcSt11char_traitsIcEEC1EP8_IO_FILESt13_Ios_Openmodem(__gnu_cxx::stdio_filebuf<char, std::char_traits<char> >* pThis, FILE* f, std::_Ios_Openmode m, unsigned long l);
 void _ZN9__gnu_cxx13stdio_filebufIcSt11char_traitsIcEEC1EP7__sFILESt13_Ios_Openmodem(__gnu_cxx::stdio_filebuf<char, std::char_traits<char> >* pThis, __darwin_FILE* f, std::_Ios_Openmode m, unsigned long l)
 {
 	_ZN9__gnu_cxx13stdio_filebufIcSt11char_traitsIcEEC1EP8_IO_FILESt13_Ios_Openmodem(pThis, f->linux_fp, m, l);
 }
+
+#elif defined(__i386__)
+
+// TODO: x86
+
+#endif
 
