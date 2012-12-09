b9c14af4d573a9903e75ae0a31d5e64271fd9cca
Lubos Dolezel <lubos@dolezel.info>
2012-12-09 11:25:28 +0100

    Still on track to fix some fstream ABI issues

--- src/libSystem/libc/stdio.cpp
+++ src/libSystem/libc/stdio.cpp
@@ -17,6 +17,10 @@
 #include <limits.h>
 #include <typeinfo>
 #include "log.h"
+#include <ext/stdio_filebuf.h>
+
+template class __gnu_cxx::stdio_filebuf<char, std::char_traits<char> >;
+template class std::basic_filebuf<char, std::char_traits<char> >;
 
 extern char g_sysroot[PATH_MAX];
 
@@ -399,5 +403,11 @@ int __darwin_remove(const char* path)
 std::__basic_file<char>* _ZNSt12__basic_fileIcE8sys_openEP7__sFILESt13_Ios_Openmode(std::__basic_file<char>* pThis, __darwin_FILE* f, std::ios_base::openmode mode)
 {
 	return pThis->sys_open(f->linux_fp, mode);
+}
+
+extern "C" void _ZN9__gnu_cxx13stdio_filebufIcSt11char_traitsIcEEC1EP8_IO_FILESt13_Ios_Openmodem(__gnu_cxx::stdio_filebuf<char, std::char_traits<char> >* pThis, FILE* f, std::_Ios_Openmode m, unsigned long l);
+void _ZN9__gnu_cxx13stdio_filebufIcSt11char_traitsIcEEC1EP7__sFILESt13_Ios_Openmodem(__gnu_cxx::stdio_filebuf<char, std::char_traits<char> >* pThis, __darwin_FILE* f, std::_Ios_Openmode m, unsigned long l)
+{
+	_ZN9__gnu_cxx13stdio_filebufIcSt11char_traitsIcEEC1EP8_IO_FILESt13_Ios_Openmodem(pThis, f->linux_fp, m, l);
 }
 
--- tests/src/fstream.cpp
+++ tests/src/fstream.cpp
@@ -1,3 +1,4 @@
+// CFLAGS: -O3
 #include <fstream>
 #include <iostream>
 #include <unistd.h>
@@ -5,27 +6,57 @@
 #include <cstdio>
 #include <ext/stdio_filebuf.h>
 
+void printFile(const char* path);
+void test1();
+void test2();
+
+char text[8001] = "";
+const char* file = "/tmp/__test123";
+
 int main()
 {
 	//__gnu_cxx::stdio_filebuf<char> out;
-	std::ifstream in;
-	const char* text = "Hello world";
+	char text[8001] = "";
 	const char* file = "/tmp/__test123";
-	char buf[100];
+
+	for (int i = 0; i < 800; i++)
+		memcpy(text+i*10, "1234567890", 11);
 	
-	//out.open(file, std::ios_base::out | std::ios_base::binary);
+	test1();
+	test2();
+
+	return 0;
+}
+
+void test1()
+{
 	__gnu_cxx::stdio_filebuf<char> out(fopen(file, "w+b"), std::ios_base::out|std::ios_base::binary);
 	out.sputn(text, strlen(text));
 	out.close();
 
-	in.open(file);
-	in.getline(buf, 100);
+	printFile(file);
+}
+
+void test2()
+{
+	std::filebuf out2;
+	out2.open(file, std::ios_base::binary|std::ios_base::in|std::ios_base::out|std::ios_base::trunc);
+	out2.sputn(text, strlen(text));
+	out2.close();
+
+	printFile(file);
+}
+
+void printFile(const char* path)
+{
+	std::ifstream in;
+	char buf[8001];
+
+	in.open(path);
+	in.getline(buf, sizeof(buf));
 	in.close();
 
-	std::cout << "sizeof: " << sizeof(out) << std::endl;
 	std::cout << buf << std::endl;
-	unlink(file);
-
-	return 0;
+	unlink(path);
 }
 
