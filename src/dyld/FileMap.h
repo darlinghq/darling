4893ac1496ce5dbeeb5a12d18851530920758d9e
Lubos Dolezel <lubos@dolezel.info>
2012-12-16 14:22:24 +0100

    Removing "Foobar" from copyright headers

--- src/dyld/FileMap.h
+++ src/dyld/FileMap.h
@@ -15,7 +15,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+along with Darling.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef FILEMAP_H
