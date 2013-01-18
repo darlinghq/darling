f9a90accfb1a615cc46b2d9368072383164480b3
Chris Wulff <chris.wulff@labxtechnologies.com>
2013-01-17 21:29:22 -0500

    A tiny bit more task_info.
    Conflicts:
    	src/libSystem/kernel-mach/task.cpp

--- src/libSystem/kernel-mach/task.cpp
+++ src/libSystem/kernel-mach/task.cpp
@@ -3,6 +3,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <cstdlib>
+#include <iostream>
+#include <execinfo.h>
 
 darwin_task_t mach_task_self_ = new darwin_task(::getpid());
 
@@ -10,4 +12,130 @@ kern_return_t mach_port_deallocate(darwin_task_t task, void* port)
 {
 	free(port);
 	return KERN_SUCCESS;
+}
+
+kern_return_t pid_for_task(mach_port_name_t t, int *pid)
+{
+	// TODO: this is supposed to be for the specified task but for now, just return our pid
+	if (pid)
+	{
+		*pid = mach_task_self_->pid;
+	}
+	return KERN_SUCCESS;
+}
+
+kern_return_t mach_port_mod_refs(ipc_space_t task, mach_port_name_t name, mach_port_right_t right, mach_port_delta_t delta)
+{
+	return KERN_SUCCESS;
+}
+
+// task_info structures
+
+#define TASK_DYLD_INFO                  17
+
+struct task_dyld_info {
+	mach_vm_address_t       all_image_info_addr;
+	mach_vm_size_t          all_image_info_size;
+	integer_t               all_image_info_format;
+};
+typedef struct task_dyld_info   task_dyld_info_data_t;
+typedef struct task_dyld_info   *task_dyld_info_t;
+#define TASK_DYLD_ALL_IMAGE_INFO_32     0
+#define TASK_DYLD_ALL_IMAGE_INFO_64     1
+
+enum dyld_image_mode { dyld_image_adding=0, dyld_image_removing=1 };
+typedef void (*dyld_image_notifier)(enum dyld_image_mode mode, uint32_t infoCount, const struct dyld_image_info info[]);
+
+struct dyld_all_image_infos {
+uint32_t                        version;
+uint32_t                        infoArrayCount;
+const struct dyld_image_info*   infoArray;
+dyld_image_notifier             notification;
+bool                            processDetachedFromSharedRegion;
+bool                            libSystemInitialized;
+const struct mach_header*       dyldImageLoadAddress;
+void*                           jitInfo;
+const char*                     dyldVersion;
+const char*                     errorMessage;
+uintptr_t                       terminationFlags;
+void*                           coreSymbolicationShmPage;
+uintptr_t                       systemOrderFlag;
+uintptr_t                       uuidArrayCount;
+const struct dyld_uuid_info*    uuidArray;
+struct dyld_all_image_infos*    dyldAllImageInfosAddress;
+uintptr_t                       initialImageCount;
+uintptr_t                       errorKind;
+const char*                     errorClientOfDylibPath;
+const char*                     errorTargetDylibPath;
+const char*                     errorSymbol;
+uintptr_t                       sharedCacheSlide;
+};
+
+struct dyld_all_image_infos dyld_all_image_infos = { 12 };
+
+kern_return_t task_info(task_name_t target_task, task_flavor_t flavor, task_info_t task_info_out, mach_msg_type_number_t *task_info_outCnt)
+{
+	std::cerr << "task_info(" << target_task << ", " << flavor << ", " << task_info_out << ", " << task_info_outCnt << " = " << *task_info_outCnt << ")" << std::endl;
+
+	switch (flavor)
+	{
+		case TASK_DYLD_INFO:
+		{
+			task_dyld_info_t dyld_info = (task_dyld_info_t)task_info_out;
+			dyld_info->all_image_info_addr = (mach_vm_address_t)&dyld_all_image_infos;
+			dyld_info->all_image_info_size = sizeof(dyld_all_image_infos);
+#ifdef __x86_64__
+			dyld_info->all_image_info_format = TASK_DYLD_ALL_IMAGE_INFO_64;
+#else
+			dyld_info->all_image_info_format = TASK_DYLD_ALL_IMAGE_INFO_32;
+#endif
+			break;
+		}
+		default:
+			// TODO: get the requested info flavor. For now, just zero the provided buffer
+			for (int i=0; i<*task_info_outCnt; i++)
+			{
+				task_info_out[i] = 0;
+			}
+			break;
+	}
+
+	return KERN_SUCCESS;
+}
+
+kern_return_t mach_vm_region_recurse(vm_map_t target_task, mach_vm_address_t *address, mach_vm_size_t *size, natural_t *nesting_depth, vm_region_recurse_info_t info, mach_msg_type_number_t *infoCnt)
+{
+	// TODO
+	*infoCnt = 0;
+	return KERN_SUCCESS;
+}
+
+int proc_name(int pid, void * buffer, uint32_t buffersize)
+{
+	// TODO: get process name
+	if (buffer)
+	{
+		*(char*)buffer = 0;
+	}
+	return 0;
+}
+
+int proc_pidpath(int pid, void * buffer, uint32_t buffersize)
+{
+	// TODO: get process path
+	if (buffer)
+	{
+		*(char*)buffer = 0;
+	}
+	return 0;
+}
+
+int proc_pidinfo(int pid, int flavor, uint64_t arg, void *buffer, int buffersize)
+{
+	// TODO: get other pid info?
+	if (buffer)
+	{
+		*(char*)buffer = 0;
+	}
+	return 0;
 }
--- src/libSystem/kernel-mach/task.h
+++ src/libSystem/kernel-mach/task.h
@@ -11,6 +11,23 @@ struct darwin_task
 
 typedef darwin_task* darwin_task_t;
 
+typedef unsigned int     natural_t;
+typedef int              integer_t;
+
+typedef natural_t        mach_port_name_t;
+typedef mach_port_name_t mach_port_t;
+typedef mach_port_t      ipc_space_t;
+typedef natural_t        mach_port_right_t;
+typedef integer_t        mach_port_delta_t;
+typedef mach_port_t      task_name_t;
+typedef natural_t        task_flavor_t;
+typedef integer_t       *task_info_t;
+typedef natural_t        mach_msg_type_number_t;
+typedef mach_port_t      vm_map_t;
+typedef uint64_t         mach_vm_address_t;
+typedef uint64_t         mach_vm_size_t;
+typedef int             *vm_region_recurse_info_t;
+
 #ifdef __cplusplus
 extern "C"
 {
@@ -18,6 +35,17 @@ extern "C"
 	extern darwin_task_t mach_task_self_;
 	inline darwin_task_t mach_task_self() { return mach_task_self_; }
 	kern_return_t mach_port_deallocate(darwin_task_t task, void* port);
+	kern_return_t pid_for_task(mach_port_name_t t, int *pid);
+	kern_return_t mach_port_mod_refs(ipc_space_t task, mach_port_name_t name, mach_port_right_t right, mach_port_delta_t delta);
+	kern_return_t task_info(task_name_t target_task, task_flavor_t flavor, task_info_t task_info_out, mach_msg_type_number_t *task_info_outCnt);
+
+	// mach_vm.h
+	kern_return_t mach_vm_region_recurse(vm_map_t target_task, mach_vm_address_t *address, mach_vm_size_t *size, natural_t *nesting_depth, vm_region_recurse_info_t info, mach_msg_type_number_t *infoCnt);
+
+	// libproc.h
+	int proc_name(int pid, void * buffer, uint32_t buffersize);
+	int proc_pidpath(int pid, void * buffer, uint32_t buffersize);
+	int proc_pidinfo(int pid, int flavor, uint64_t arg, void *buffer, int buffersize);
 #ifdef __cplusplus
 }
 #endif
