e9406fd8d2f1313993f56db43b3b4616be49ceeb
Lubos Dolezel <lubos@dolezel.info>
2013-01-24 16:42:54 +0100

    dyld-multilib.c: support for decision making on fat binaries

--- src/dyld/dyld-multilib.c
+++ src/dyld/dyld-multilib.c
@@ -7,8 +7,19 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+struct fat_arch
+{
+	uint32_t cputype;
+	uint32_t cpusubtype;
+	uint32_t offset;
+	uint32_t size;
+	uint32_t align;
+};
 
 void junction(const char* target, int argc, char** argv);
+const char* decideFat(int fd, bool swapEndian);
 
 int main(int argc, char** argv)
 {
@@ -16,7 +27,7 @@ int main(int argc, char** argv)
 	int fd;
 	const char* target = "64";
 
-	fd = open(argv[1], O_RDONLY);
+	fd = open(argv[1], O_RDONLY | O_CLOEXEC);
 
 	// We let real dyld output all serious error messages
 	// not to duplicate the functionality.
@@ -26,8 +37,10 @@ int main(int argc, char** argv)
 	if (read(fd, &signature, 4) != 4)
 		junction(target, argc, argv);
 
-	if (signature == 0xfeedface)
+	if (signature == 0xfeedface || signature == 0xcefaedfe)
 		target = "32";
+	else if (signature == 0xcafebabe || signature == 0xbebafeca)
+		target = decideFat(fd, signature == 0xbebafeca);
 
 	junction(target, argc, argv);
 	return 1;
@@ -48,5 +61,40 @@ void junction(const char* target, int argc, char** argv)
 	free(targetPath);
 
 	exit(errno);
+}
+
+const char* decideFat(int fd, bool swapEndian)
+{
+	uint32_t nArchs;
+	struct fat_arch* archs;
+	size_t bytes;
+	const char* rv = "32";
+	
+	if (read(fd, &nArchs, 4) != 4)
+		return "64";
+	
+	if (swapEndian)
+		nArchs = __builtin_bswap32(nArchs);
+	
+	bytes = nArchs*sizeof(struct fat_arch);
+	archs = (struct fat_arch*) malloc(bytes);
+	
+	if (read(fd, archs, bytes) != bytes)
+		return "64";
+	
+	for (uint32_t i = 0; i < nArchs; i++)
+	{
+		uint32_t cputype = archs[i].cputype;
+		
+		if (swapEndian)
+			cputype = __builtin_bswap32(cputype);
+		
+		if (cputype & 0x01000000) // 64-bit
+			rv = "64";
+	}
+	
+	free(archs);
+	
+	return rv;
 }
 
