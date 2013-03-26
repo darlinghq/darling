4ab65d0b27c80f7967f41f436202614aff5dda4b
Lubos Dolezel <lubos@dolezel.info>
2013-03-26 13:02:07 +0100

    Adding a missing header file: AppleCFString.h

---
+++ src/CoreFoundation/AppleCFString.h
@@ -0,0 +1,34 @@
+#ifndef APPLECFSTRING_H
+#define APPLECFSTRING_H
+#include <stdint.h>
+#include <limits.h>
+#import <Foundation/NSString.h>
+
+#pragma pack(1)
+@interface AppleCFString : NSString
+{
+	uint8_t _flags;
+	uint8_t _typeID;
+	uint8_t _unused[2];
+#ifdef __x86_64__
+	uint32_t _extraFlags;
+#endif
+	union
+	{
+		const unichar* _unicodeData;
+		const char* _asciiData;
+	} _data;
+	uintptr_t _length;
+};
+
+- (NSUInteger)length;
+- (unichar)characterAtIndex:(NSUInteger)index;
+- (void)getCharacters:(unichar *)buffer range:(NSRange)aRange;
+- (const char *)UTF8String;
+@end
+#pragma pack()
+
+enum { apple_immutable = 0x80, apple_notinline = 0x40, apple_unicode = 0x20, apple_hasnull = 0x10, apple_haslength = 8 };
+
+#endif
+
