c539a83070d45ad2bd5711bc868f4a6e238550bf
Lubos Dolezel <lubos@dolezel.info>
2014-01-10 21:19:08 +0100

    Initial XEvents mapping in AppKit

--- src/AppKit/NSX11Event.mm
+++ src/AppKit/NSX11Event.mm
@@ -24,8 +24,41 @@
 {
 	switch (m_event.type)
 	{
+		case KeyPress:
+			return NSKeyDown;
+		case KeyRelease:
+			return NSKeyUp;
+		case ButtonPress:
+			switch (m_event.xbutton.button)
+			{
+				case 1:
+					return NSLeftMouseDown;
+				case 3:
+					return NSRightMouseDown;
+				case 4: // scroll up
+				case 5: // scroll down
+					return NSScrollWheel;
+				default:
+					return NSOtherMouseDown;
+			}
+		case ButtonRelease:
+			switch (m_event.xbutton.button)
+			{
+				case 1:
+					return NSLeftMouseUp;
+				case 3:
+					return NSRightMouseUp;
+				default:
+					return NSOtherMouseUp;
+			}
+		case EnterNotify:
+			return NSMouseEntered;
+		case LeaveNotify:
+			return NSMouseExited;
+		case MotionNotify:
+			return NSMouseMoved; // TODO: NSLeftMouseDragged / NSRightMouseDragged
 		default:
-			return 0;
+			return NSAppKitDefined;
 	}
 }
 
