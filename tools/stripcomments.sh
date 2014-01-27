dcb213e1d260daa4ca3e0b0b4b5f0163d759b870
Lubos Dolezel <lubos@dolezel.info>
2014-01-27 13:56:48 +0100

    Added tools/stripcomments.sh for easy header file clensing

---
+++ tools/stripcomments.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# This cool can be used to strip most of the copyrightable material from header files.
+# Remember that all files processed with this script stil need to be checked manually!
+
+gcc -DNS_BLOCKS_AVAILABLE -fpreprocessed -dD -E "$1" | egrep -v '# [[:digit:]]' | sed -r 's/APPKIT_EXTERN/EXTERN_C/;s/ ?(NS_AVAILABLE(_MAC)?|NS_DEPRECATED_MAC|NS_CLASS_AVAILABLE)\([^)]+\)//;s/#pragma mark.*//'
+
