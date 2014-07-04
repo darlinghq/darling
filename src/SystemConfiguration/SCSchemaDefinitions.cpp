748ddb5211a2d10334dd774af39fdc90a827584b
Lubos Dolezel <lubos@dolezel.info>
2014-07-04 07:51:03 +0200

    SystemConfiguration: more constants

---
+++ src/SystemConfiguration/SCSchemaDefinitions.cpp
@@ -0,0 +1,28 @@
+#include "SCSchemaDefinitions.h"
+
+const CFStringRef kSCPropNetProxiesExceptionsList = CFSTR("ExceptionsList");
+const CFStringRef kSCPropNetProxiesExcludeSimpleHostnames = CFSTR("ExcludeSimpleHostnames");
+const CFStringRef kSCPropNetProxiesFTPEnable = CFSTR("FTPEnable");
+const CFStringRef kSCPropNetProxiesFTPPassive = CFSTR("FTPPassive");
+const CFStringRef kSCPropNetProxiesFTPPort = CFSTR("FTPPort");
+const CFStringRef kSCPropNetProxiesFTPProxy = CFSTR("FTPProxy");
+const CFStringRef kSCPropNetProxiesGopherEnable = CFSTR("GopherEnable");
+const CFStringRef kSCPropNetProxiesGopherPort = CFSTR("GopherPort");
+const CFStringRef kSCPropNetProxiesGopherProxy = CFSTR("GopherProxy");
+const CFStringRef kSCPropNetProxiesHTTPEnable = CFSTR("HTTPEnable");
+const CFStringRef kSCPropNetProxiesHTTPPort = CFSTR("HTTPPort");
+const CFStringRef kSCPropNetProxiesHTTPProxy = CFSTR("HTTPProxy");
+const CFStringRef kSCPropNetProxiesHTTPSEnable = CFSTR("HTTPSEnable");
+const CFStringRef kSCPropNetProxiesHTTPSPort = CFSTR("HTTPSPort");
+const CFStringRef kSCPropNetProxiesHTTPSProxy = CFSTR("HTTPSProxy");
+const CFStringRef kSCPropNetProxiesRTSPEnable = CFSTR("RTSPEnable");
+const CFStringRef kSCPropNetProxiesRTSPPort = CFSTR("RTSPPort");
+const CFStringRef kSCPropNetProxiesRTSPProxy = CFSTR("RTSPProxy");
+const CFStringRef kSCPropNetProxiesSOCKSEnable = CFSTR("SOCKSEnable");
+const CFStringRef kSCPropNetProxiesSOCKSPort = CFSTR("SOCKSPort");
+const CFStringRef kSCPropNetProxiesSOCKSProxy = CFSTR("SOCKSProxy");
+const CFStringRef kSCPropNetProxiesProxyAutoConfigEnable = CFSTR("ProxyAutoConfigEnable");
+const CFStringRef kSCPropNetProxiesProxyAutoConfigJavaScript = CFSTR("ProxyAutoConfigJavaScript");
+const CFStringRef kSCPropNetProxiesProxyAutoConfigURLString = CFSTR("ProxyAutoConfigURLString");
+const CFStringRef kSCPropNetProxiesProxyAutoDiscoveryEnable = CFSTR("ProxyAutoDiscoveryEnable");
+
---
+++ src/SystemConfiguration/SCSchemaDefinitions.h
@@ -0,0 +1,40 @@
+#ifndef SCSCHEMADEFINITIONS_H
+#define SCSCHEMADEFINITIONS_H
+#include <CoreFoundation/CFString.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const CFStringRef kSCPropNetProxiesExceptionsList;
+extern const CFStringRef kSCPropNetProxiesExcludeSimpleHostnames;
+extern const CFStringRef kSCPropNetProxiesFTPEnable;
+extern const CFStringRef kSCPropNetProxiesFTPPassive;
+extern const CFStringRef kSCPropNetProxiesFTPPort;
+extern const CFStringRef kSCPropNetProxiesFTPProxy;
+extern const CFStringRef kSCPropNetProxiesGopherEnable;
+extern const CFStringRef kSCPropNetProxiesGopherPort;
+extern const CFStringRef kSCPropNetProxiesGopherProxy;
+extern const CFStringRef kSCPropNetProxiesHTTPEnable;
+extern const CFStringRef kSCPropNetProxiesHTTPPort;
+extern const CFStringRef kSCPropNetProxiesHTTPProxy;
+extern const CFStringRef kSCPropNetProxiesHTTPSEnable;
+extern const CFStringRef kSCPropNetProxiesHTTPSPort;
+extern const CFStringRef kSCPropNetProxiesHTTPSProxy;
+extern const CFStringRef kSCPropNetProxiesRTSPEnable;
+extern const CFStringRef kSCPropNetProxiesRTSPPort;
+extern const CFStringRef kSCPropNetProxiesRTSPProxy;
+extern const CFStringRef kSCPropNetProxiesSOCKSEnable;
+extern const CFStringRef kSCPropNetProxiesSOCKSPort;
+extern const CFStringRef kSCPropNetProxiesSOCKSProxy;
+extern const CFStringRef kSCPropNetProxiesProxyAutoConfigEnable;
+extern const CFStringRef kSCPropNetProxiesProxyAutoConfigJavaScript;
+extern const CFStringRef kSCPropNetProxiesProxyAutoConfigURLString;
+extern const CFStringRef kSCPropNetProxiesProxyAutoDiscoveryEnable;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
