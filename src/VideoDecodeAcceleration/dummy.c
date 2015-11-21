763d41cf67d3df165c8fbfb52abe8b672c203d9f
Lubos Dolezel <lubos@dolezel.info>
2015-11-21 22:59:34 +0100

    Add VDA constants, add dummy VDA into dylib.conf

--- src/VideoDecodeAcceleration/dummy.c
+++ src/VideoDecodeAcceleration/dummy.c
@@ -1,6 +1,13 @@
 #include <CoreFoundation/CFBase.h>
+#include <CoreFoundation/CFString.h>
+#include "../CFF/CFStringConst.h"
 
 // A real implementation would use VA API or VDPAU
+
+CONST_STRING_DECL(kVDADecoderConfiguration_Height, "kVDADecoderConfiguration_Height");
+CONST_STRING_DECL(kVDADecoderConfiguration_Width, "kVDADecoderConfiguration_Width");
+CONST_STRING_DECL(kVDADecoderConfiguration_SourceFormat, "kVDADecoderConfiguration_SourceFormat");
+CONST_STRING_DECL(kVDADecoderConfiguration_avcCData, "kVDADecoderConfiguration_avcCData");
 
 #define kVDADecoderHardwareNotSupportedErr -12470
 
