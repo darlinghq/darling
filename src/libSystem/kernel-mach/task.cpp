a8cfec3d01e795e0a36ffa3dd87bcecbd750ac29
neon12345 <alexanderhochbaum@web.de>
2015-12-02 02:02:41 +0100

    Update task.cpp
    Don't know what's going on here but the free error could be related to now fixed "bugs".

--- src/libSystem/kernel-mach/task.cpp
+++ src/libSystem/kernel-mach/task.cpp
@@ -12,7 +12,7 @@ darwin_task_t mach_task_self_ = new darwin_task(::getpid());
 
 kern_return_t mach_port_deallocate(darwin_task_t task, void* port)
 {
-	//free(port);
+	free(port);
 	return KERN_SUCCESS;
 }
 
