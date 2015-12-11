9897e0e2224f70bdb2ede9edd912e0e8d479f257
Lubos Dolezel <lubos@dolezel.info>
2015-12-11 09:56:11 +0100

    Implement _dirhelper() for clang (#102)

--- src/duct/src/dirhelper.c
+++ src/duct/src/dirhelper.c
@@ -1,12 +1,58 @@
 #include <errno.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void _dirhelper_fork_child(void)
 {
 }
 
-int _dirhelper()
+enum {
+	DIRHELPER_USER_LOCAL = 0,
+	DIRHELPER_USER_LOCAL_TEMP,
+	DIRHELPER_USER_LOCAL_CACHE
+};
+
+char* _dirhelper(int which, char* p, size_t limit)
 {
-	errno = ENOSYS;
-	return -1;
+	switch (which)
+	{
+		case DIRHELPER_USER_LOCAL:
+		{
+			const char* home;
+
+			home = getenv("HOME");
+			if (home == NULL)
+				home = "/";
+
+			strlcpy(p, home, limit);
+			return p;
+		}
+		case DIRHELPER_USER_LOCAL_TEMP:
+		{
+			const char* tmp;
+
+			tmp = getenv("TMPDIR");
+			if (tmp == NULL)
+				tmp = "/tmp";
+
+			strlcpy(p, tmp, limit);
+			return p;
+		}
+		case DIRHELPER_USER_LOCAL_CACHE:
+		{
+			const char* home;
+
+			home = getenv("HOME");
+			if (home != NULL)
+				snprintf(p, limit, "%s/.cache", home);
+			else
+				strlcpy(p, "/tmp", limit);
+
+			return p;
+		}
+		default:
+			return NULL;
+	}
 }
 
