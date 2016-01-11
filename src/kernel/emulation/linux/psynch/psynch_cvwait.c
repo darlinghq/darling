74e3c66636ee5c3ba10e5a879807123950f18632
Lubos Dolezel <lubos@dolezel.info>
2016-01-11 15:23:10 +0100

    Support >6 arg syscalls properly

--- src/kernel/emulation/linux/psynch/psynch_cvwait.c
+++ src/kernel/emulation/linux/psynch/psynch_cvwait.c
@@ -6,10 +6,8 @@
 #include "../../../../lkm/api.h"
 #include "../simple.h"
 
-// FIXME: __hack = result of 'call' instruction used to invoke Darling syscalls
-
 long sys_psynch_cvwait(void* cv, uint32_t cvgen, uint32_t cvugen, void* mutex, uint32_t mgen,
-		uint32_t ugen, void* __hack, uint64_t sec, uint64_t usec)
+		uint32_t ugen, uint64_t sec, uint64_t usec)
 {
 	struct psynch_cvwait_args args = {
 		.cv = cv,
