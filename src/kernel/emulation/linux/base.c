e15126df29c066fff6c39036c19edb5cebb3cd27
Luboš Doležel <lubos@dolezel.info>
2016-01-29 09:02:10 +0100

    Move out linux_syscall(), because some old versions of Clang generate crap prolog in naked functions

--- src/kernel/emulation/linux/base.c
+++ src/kernel/emulation/linux/base.c
@@ -5,53 +5,6 @@
 #include "duct_errno.h"
 #include "syscalls.h"
 
-#ifdef __x86_64__
-__attribute__((naked))
-long linux_syscall(long a1, long a2, long a3, long a4, long a5, long a6, int nr)
-{
-	__asm__ ("movl 8(%rsp), %eax\n"
-			"movq %rcx, %r10\n"
-			"syscall\n"
-			"ret");
-}
-#elif defined(__i386__)
-__attribute__((naked))
-long linux_syscall(long a1, long a2, long a3, long a4, long a5, long a6, int nr)
-{
-	__asm__("push %ebp\n"
-			"push %ebx\n"
-			"push %esi\n"
-			"push %edi\n"
-			"call 1f\n"
-			"1:\n"
-			"pop %ebp\n"
-			"addl $(2f-1b), %ebp\n"
-			"push %ebp\n"
-			"push %ecx\n"
-			"push %edx\n"
-			"movl 28+28(%esp), %eax\n"
-			"movl 4+28(%esp), %ebx\n"
-			"movl 8+28(%esp), %ecx\n"
-			"movl 12+28(%esp), %edx\n"
-			"movl 16+28(%esp), %esi\n"
-			"movl 20+28(%esp), %edi\n"
-			"push %ebp\n"
-			"movl 24+28(%esp), %ebp\n"
-			"movl %ebp, -4(%esp)\n"
-			"movl %esp, %ebp\n"
-			"sysenter\n"
-			"2:\n"
-			"pop %edi\n"
-			"pop %esi\n"
-			"pop %ebx\n"
-			"pop %ebp\n"
-			"ret"
-		   );
-}
-#else
-#	error Unsupported platform!
-#endif
-
 long __unknown_syscall(int nr, ...)
 {
 	__simple_printf("Unimplemented syscall (%d)\n", nr);
