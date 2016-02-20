c56240325bd9da15a862781e2c4b3b403ad3f830
Lubos Dolezel <lubos@dolezel.info>
2016-02-20 23:42:01 +0100

    Several fixes for fake sudo

--- src/kernel/emulation/linux/unistd/getgroups.c
+++ src/kernel/emulation/linux/unistd/getgroups.c
@@ -2,10 +2,20 @@
 #include "../base.h"
 #include "../errno.h"
 #include <asm/unistd.h>
+#include <stddef.h>
 
 long sys_getgroups(unsigned int size, int* gidset)
 {
 	int ret;
+	
+	if (getenv("__FAKE_SETUID_ROOT") != NULL)
+	{
+		if (size >= 1)
+		{
+			gidset[0] = 0;
+			return 1;
+		}
+	}
 
 #ifdef __NR_getgroups32
 	ret = LINUX_SYSCALL(__NR_getgroups32, size, gidset);
