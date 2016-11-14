45b702c5e1da3d87f9bad78f98b6d21112216d2c
Lubos Dolezel <lubos@dolezel.info>
2016-11-14 15:08:07 +0100

    Add _CSCheckFix() into CoreServices, required by Security.framework

---
+++ src/CoreServices/CoreServicesPriv.h
@@ -0,0 +1,11 @@
+#ifndef CORESERVICESPRIV_H_
+#define CORESERVICESPRIV_H_
+#include <CoreFoundation/CFString.h>
+
+CF_EXTERN_C_BEGIN
+
+Boolean _CSCheckFix(CFStringRef str);
+
+CF_EXTERN_C_END
+
+#endif
