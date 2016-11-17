cf658648fa3a1ad7887d26aeb61598031d5a9879
Lubos Dolezel <lubos@dolezel.info>
2016-11-17 18:02:44 +0100

    Add OpenScriptingUtilPriv.h dummy for Security

---
+++ src/OpenScripting/OpenScriptingUtilPriv.h
@@ -0,0 +1,11 @@
+#ifndef OPENSCRIPTINGUTILPRIV_H_
+#define OPENSCRIPTINGUTILPRIV_H_
+#include <CoreFoundation/CFURL.h>
+
+static inline BOOL OSAIsRecognizedExecutableURL(CFURLRef url, SInt32* error)
+{
+	return NO;
+}
+
+#endif
+
