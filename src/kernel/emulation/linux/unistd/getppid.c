498feb952235075ac00c1d341ba98e7705330ef1
Lubos Dolezel <lubos@dolezel.info>
2016-11-24 10:42:30 +0100

    getppid() should return 1 instead of 0 when getpid() == 1

--- src/kernel/emulation/linux/unistd/getppid.c
+++ src/kernel/emulation/linux/unistd/getppid.c
@@ -11,6 +11,12 @@ long sys_getppid(void)
 	if (ret < 0)
 		ret = errno_linux_to_bsd(ret);
 
+	// The PID 1 process in a Linux PID namespace has PID 1, but PPID 0.
+	// This fixes the PPID value to 1 (which is what a real PID 1 process gets).
+	// Apple's launchd requires this.
+	if (ret == 0)
+		ret = 1;
+
 	return ret;
 }
 
