07cf799252fb55dfbd60be228e39fae30db7a8dc
Andrew Hyatt <ahyattdev@icloud.com>
2017-01-01 13:50:40 -0800

    Fix dkms.sh Before it would only build if it was never run before. Now, it rebuilds every time.

--- src/lkm/dkms.sh
+++ src/lkm/dkms.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-export DARLING_SRC=`pwd`
-dkms add .
-dkms build -m darling-mach -v 1.0
-dkms install -m darling-mach -v 1.0
 
+export DARLING_SRC=`pwd`
+VERSION=1.0
+
+# Test if darling-mach is already installed
+modinfo darling-mach &> /dev/null
+
+if (( $? == 0 )); then
+  dkms remove darling-mach/$VERSION --all
+fi
+
+# Build it from scratch and install
+dkms add .
+dkms build darling-mach -v $VERSION
+dkms install darling-mach -v $VERSION
