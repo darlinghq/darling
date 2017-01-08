dce27e3d6017e4a5bf5865616142ab230e6082f0
Sergey Bugaev <bugaevc@gmail.com>
2017-01-08 21:03:49 +0300

    Quote "/" inside a character class in regex
    Apparently, some versions of awk require this and some don't.

--- src/setup-ld-so.sh
+++ src/setup-ld-so.sh
@@ -6,7 +6,7 @@
 for file in /etc/ld.so.conf $(find /etc/ld.so.conf.d/ -type f); do
     # Copy lines from e.g. /etc/ld.so.conf into ./etc/ld.so.conf,
     # prepending "/Volumes/SystemRoot" to each line that starts with a slash
-    awk '/^\// { print "/Volumes/SystemRoot" $0 }; /^[^/]/' $file > .$file
+    awk '/^\// { print "/Volumes/SystemRoot" $0 }; /^[^\/]/' $file > .$file
 done
 
 unshare --mount bash -c "mount --rbind / Volumes/SystemRoot && ldconfig -r . -X"
