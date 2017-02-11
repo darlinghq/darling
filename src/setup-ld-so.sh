e1f8d6f374f28e57af08abe8bdaecfeb54a2af6f
Sergey Bugaev <bugaevc@gmail.com>
2017-02-11 12:50:57 +0300

    Build QuartzCore

--- src/setup-ld-so.sh
+++ src/setup-ld-so.sh
@@ -3,7 +3,7 @@
 # This runs as root,
 # with the current directory set to ${CMAKE_INSTALL_PREFIX}/libexec/darling
 
-for file in /etc/ld.so.conf $(find /etc/ld.so.conf.d/ -type f); do
+for file in /etc/ld.so.conf $(find /etc/ld.so.conf.d/ -type f -o -type l); do
     # Copy lines from e.g. /etc/ld.so.conf into ./etc/ld.so.conf,
     # prepending "/Volumes/SystemRoot" to each line that starts with a slash
     awk '/^\// { print "/Volumes/SystemRoot" $0 }; /^[^\/]/' $file > .$file
