53ff62014b15f7da82548b05fe63affd404e1dec
Lubos Dolezel <lubos@dolezel.info>
2017-03-01 18:58:20 +0100

    Release kqueues properly

--- src/kernel/emulation/linux/unistd/close.c
+++ src/kernel/emulation/linux/unistd/close.c
@@ -3,6 +3,10 @@
 #include "../errno.h"
 #include <linux-syscalls/linux.h>
 
+__attribute__((weak))
+__attribute__((visibility("default")))
+int kqueue_close(int kq) { return -1; }
+
 long sys_close(int fd)
 {
 	return sys_close_nocancel(fd);
@@ -15,6 +19,8 @@ long sys_close_nocancel(int fd)
 	ret = LINUX_SYSCALL1(__NR_close, fd);
 	if (ret < 0)
 		ret = errno_linux_to_bsd(ret);
+	else
+		kqueue_close(fd);
 
 	return ret;
 }
