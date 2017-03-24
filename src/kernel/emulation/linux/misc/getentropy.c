596aea937bf5aa6b5af1e0b706320f7a2be91dac
Lubos Dolezel <lubos@dolezel.info>
2017-03-24 21:37:12 +0100

    Do not use GRND_RANDOM in getentropy()

--- src/kernel/emulation/linux/misc/getentropy.c
+++ src/kernel/emulation/linux/misc/getentropy.c
@@ -9,7 +9,7 @@ long sys_getentropy(void* buf, unsigned long size)
 {
 	int ret;
 
-	ret = LINUX_SYSCALL(__NR_getrandom, buf, size, LINUX_GRND_RANDOM);
+	ret = LINUX_SYSCALL(__NR_getrandom, buf, size, 0);
 	if (ret < 0)
 		ret = errno_linux_to_bsd(ret);
 
