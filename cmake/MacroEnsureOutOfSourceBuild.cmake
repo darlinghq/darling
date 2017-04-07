b2f23864f79fefcd09e65218fb3792023e556312
Lubos Dolezel <lubos@dolezel.info>
2017-04-07 18:48:16 +0200

    LKM build from CMake (resolves #280)

---
+++ cmake/MacroEnsureOutOfSourceBuild.cmake
@@ -0,0 +1,14 @@
+macro( MACRO_ENSURE_OUT_OF_SOURCE_BUILD )
+string( COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" _insource )
+if( _insource )
+ message( FATAL_ERROR
+ "In-source builds are not allowed.
+ CMake would overwrite the Linux kernel module Makefile.
+ Please create a directory and run cmake from there, passing the path
+ to this source directory as the last argument.
+ This process created the file `CMakeCache.txt' and the directory `CMakeFiles'.
+ Please delete them."
+ )
+endif( _insource )
+endmacro( MACRO_ENSURE_OUT_OF_SOURCE_BUILD )
+
