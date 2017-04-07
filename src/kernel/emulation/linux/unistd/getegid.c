a9abe47e0d761598390f7a7a8f095460d6a3fec1
Andrew Hyatt <ahyattdev@icloud.com>
2017-04-07 08:31:04 -0700

    Fake the GID too

--- src/kernel/emulation/linux/unistd/getegid.c
+++ src/kernel/emulation/linux/unistd/getegid.c
@@ -2,10 +2,17 @@
 #include "../base.h"
 #include "../errno.h"
 #include <linux-syscalls/linux.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 long sys_getegid(void)
 {
 	int ret;
+
+#ifndef VARIANT_DYLD // dyld cannot call getenv
+        if (getenv("__FAKE_SETGID_ROOT") != NULL)
+                return 0;
+#endif
 
 #ifdef __NR_getegid32
 	ret = LINUX_SYSCALL0(__NR_getegid32);
