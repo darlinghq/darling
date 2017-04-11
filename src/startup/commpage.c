01638947a13e1be210cc0afc8b61294f4e72fe59
Lubos Dolezel <lubos@dolezel.info>
2017-04-11 16:38:26 +0200

    Our new libdispatch doesn't crash immediately

--- src/startup/commpage.c
+++ src/startup/commpage.c
@@ -28,6 +28,7 @@ void commpage_setup(bool _64bit)
 	char* signature;
 	uint64_t my_caps;
 	uint8_t *ncpus, *nactivecpus;
+	uint8_t *physcpus, *logcpus;
 	struct sysinfo si;
 
 	commpage = (uint8_t*) mmap((void*)(_64bit ? _COMM_PAGE64_BASE_ADDRESS : _COMM_PAGE32_BASE_ADDRESS),
@@ -51,6 +52,11 @@ void commpage_setup(bool _64bit)
 
 	nactivecpus = (uint8_t*)CGET(_COMM_PAGE_ACTIVE_CPUS);
 	*nactivecpus = sysconf(_SC_NPROCESSORS_ONLN);
+
+	// Better imprecise information than no information
+	physcpus = (uint8_t*)CGET(_COMM_PAGE_PHYSICAL_CPUS);
+	logcpus = (uint8_t*)CGET(_COMM_PAGE_LOGICAL_CPUS);
+	*physcpus = *logcpus = *ncpus;
 
 	my_caps = get_cpu_caps();
 	if (*ncpus == 1)
