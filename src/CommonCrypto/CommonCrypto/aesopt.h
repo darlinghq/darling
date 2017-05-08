1fcef492989efe1634d23c6efb45b76967751581
Andrew Hyatt <ahyattdev@icloud.com>
2017-05-07 18:02:26 -0700

    Remove the old CommonCrypto

--- src/CommonCrypto/CommonCrypto/aesopt.h
+++
@@ -1,1 +0,0 @@
-../Source/CommonCryptoSPI/aesopt.h
--- src/CommonCrypto/Source/libtomcrypt/src/ciphers/aesedpport/Context.h
+++
@@ -1,9 +0,0 @@
-// Define byte offset of key within context structure.
-#define	ContextKey			0
-
-/*	Define byte offset of key length within context structure.  The number
-	stored there is the number of bytes from the start of the first round key
-	to the start of the last round key.  That is 16 less than the number of
-	bytes in the entire key.
-*/
-#define	ContextKeyLength	240
