e9eace38884b00926b19563e4575b5f6a51f9652
Lubos Dolezel <lubos@dolezel.info>
2017-05-16 22:33:23 +0200

    Improve stub generator, add three stub frameworks for xcodebuild

---
+++ src/AppleSystemInfo/functions.h
@@ -0,0 +1,41 @@
+/*
+This file is part of Darling.
+
+Copyright (C) 2017 Lubos Dolezel
+    
+Darling is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+ 
+Darling is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Darling.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+void* ASI_BusSpeedInHz(void);
+void* ASI_CPUSpeedInHz(void);
+void* ASI_CopyBusSpeedDescription(void);
+void* ASI_CopyCPUDescription(void);
+void* ASI_CopyCPUKind(void);
+void* ASI_CopyComputerModelName(void);
+void* ASI_CopyCurrentBusSpeedDescription(void);
+void* ASI_CopyCurrentCPUDescription(void);
+void* ASI_CopyCurrentRAMDescription(void);
+void* ASI_CopyCurrentRAMDescriptionWithType(void);
+void* ASI_CopyDiskSizeDescription(void);
+void* ASI_CopyFormattedSerialNumber(void);
+void* ASI_CopyHostUUID(void);
+void* ASI_CopyMachineConfigurationCode(void);
+void* ASI_CopyRAMDescription(void);
+void* ASI_IsPlatformFeatureEnabled(void);
+void* ASI_NumberOfCPUs(void);
+void* ASI_PhysicalMemorySize(void);
+void* CopyIOPlatformSerialNumberString(void);
+void* CopyPropertyAsData(void);
+void* GetIOPlatformFeature(void);
+void* PlatformOverideExistsAndIsSet(void);
