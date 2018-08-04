3f7d4758b9de86b9452c7caa5fa2c178f33b44ac
Andrew Hyatt <ahyattdev@icloud.com>
2018-08-04 13:21:58 -0400

    Xcodeproj fixes (#144)

--- cmake/xcproj.cmake
+++ cmake/xcproj.cmake
@@ -1,3 +1,11 @@
+# Create developer dir symlinks
+
+add_custom_target(xcproj_symlinks
+	DEPENDS Foundation
+	COMMAND env BUILD=${CMAKE_BUID_DIR} ${CMAKE_SOURCE_DIR}/Developer/symlinks.sh
+	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/Developer
+)
+
 function(xcproj)
 	set(options)
 	set(oneValueArgs PROJECT)
@@ -6,21 +14,27 @@ function(xcproj)
 
 	set(product_dir ${CMAKE_CURRENT_BINARY_DIR}/Build/Products/Release)
 	
-	foreach(executable ${xcproj_EXECUTABLES})
+	foreach(executable IN LISTS xcproj_EXECUTABLES)
 		set(output ${output} ${product_dir}/${executable})
 	endforeach()
 
-	foreach(library ${xcproj_LIBRARIES})
+	foreach(library IN LISTS xcproj_LIBRARIES)
 		set(output ${output} ${product_dir}/${library}.dylib)
 	endforeach()
 
-	foreach(framework ${xcproj_FRAMEWORKS})
+	foreach(framework IN LISTS xcproj_FRAMEWORKS)
 		set(output ${output} ${product_dir}/${framework}.framework/${framework})
 	endforeach()
 
 	add_custom_command(
 		OUTPUT ${output}
-		COMMAND env RUNTIME_SPEC_PATH=${CMAKE_SOURCE_DIR}/src/external/xcbuild/Specifications/ DEVELOPER_DIR=${CMAKE_SOURCE_DIR}/Developer/ ${CMAKE_BINARY_DIR}/src/external/xcbuild/xcodebuild -project ${xcproj_PROJECT}
-		DEPENDS xcbuild
+		COMMAND env RUNTIME_SPEC_PATH=${CMAKE_SOURCE_DIR}/src/external/xcbuild/Specifications DEVELOPER_DIR=${CMAKE_SOURCE_DIR}/Developer ${CMAKE_BINARY_DIR}/src/external/xcbuild/Libraries/xcdriver/xcodebuild -project ${CMAKE_CURRENT_SOURCE_DIR}/${xcproj_PROJECT} -derivedDataPath ${CMAKE_CURRENT_BINARY_DIR}
+		DEPENDS xcbuild xcproj_symlinks
 	)
+
+	foreach(framework IN LISTS xcproj_FRAMEWORKS)
+		add_custom_target(${framework} ALL
+			DEPENDS ${product_dir}/${framework}.framework/${framework}
+		)
+	endforeach()
 endfunction(xcproj)
