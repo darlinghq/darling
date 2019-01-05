6605e8d1f01ee9e26178b0aa3d1f53e13475dad0
Andrew Hyatt <ahyattdev@icloud.com>
2019-01-05 17:23:51 -0500

    Stubs for Xcode 9/10 (#445)

---
+++ src/CoreServices/constants.m
@@ -0,0 +1,8 @@
+#include <CoreFoundation/CoreFoundation.h>
+#include <Foundation/Foundation.h>
+
+const CFStringRef *_kLSPIDKey = CFSTR("pid");
+const CFStringRef *kUTTagClassDeviceModelCode = CFSTR("com.apple.device-model-code");
+const CFStringRef *kUTTagClassFilenameExtension = CFSTR("public.filename-extension");
+const CFStringRef *kUTTagClassMIMEType = CFSTR("public.mime-type");
+const CFStringRef *kUTTypeDevice = CFSTR("public.device");
