3cb6eb5f454226fb66ee342dac0bc8429c44093c
Andrew Hyatt <ahyattdev@icloud.com>
2019-01-06 02:03:05 -0500

    Symbols for Xcode 9/10 (#445)

--- src/ApplicationServices/ApplicationServices.c
+++ src/ApplicationServices/ApplicationServices.c
@@ -1,3 +1,10 @@
+#include <CoreFoundation/CoreFoundation.h>
+
 const char* ApplicationServicesVersionString = "Darling ApplicationServices-48";
 const unsigned long long ApplicationServicesVersionNumber = 0x4048000000000000;
-
+const CFStringRef kAXUIElementCopyHierarchyArrayAttributesKey = CFSTR("AXCHAA");
+const CFStringRef kAXUIElementCopyHierarchyMaxArrayCountKey = CFSTR("AXCHMAC");
+const CFStringRef kAXUIElementCopyHierarchyResultValueKey = CFSTR("value");
+const CFStringRef kAXUIElementCopyHierarchyReturnAttributeErrorsKey = CFSTR("AXCHRE");
+const CFStringRef kAXUIElementCopyHierarchySkipInspectionForAttributesKey = CFSTR("AXCHSIA");
+const CFStringRef kAXUIElementCopyHierarchyTruncateStringsKey = CFSTR("AXTRUNC");
--- src/CoreServices/constants.m
+++ src/CoreServices/constants.m
@@ -1,8 +1,7 @@
 #include <CoreFoundation/CoreFoundation.h>
-#include <Foundation/Foundation.h>
 
-const CFStringRef *_kLSPIDKey = CFSTR("pid");
-const CFStringRef *kUTTagClassDeviceModelCode = CFSTR("com.apple.device-model-code");
-const CFStringRef *kUTTagClassFilenameExtension = CFSTR("public.filename-extension");
-const CFStringRef *kUTTagClassMIMEType = CFSTR("public.mime-type");
-const CFStringRef *kUTTypeDevice = CFSTR("public.device");
+const CFStringRef _kLSPIDKey = CFSTR("pid");
+const CFStringRef kUTTagClassDeviceModelCode = CFSTR("com.apple.device-model-code");
+const CFStringRef kUTTagClassFilenameExtension = CFSTR("public.filename-extension");
+const CFStringRef kUTTagClassMIMEType = CFSTR("public.mime-type");
+const CFStringRef kUTTypeDevice = CFSTR("public.device");
