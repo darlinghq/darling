d0c42cc83e57454e0de073240639044aa96f466d
Andrew Hyatt <ahyattdev@icloud.com>
2019-01-16 11:35:55 -0500

    LaunchServices constants (#445)

--- src/CoreServices/constants.m
+++ src/CoreServices/constants.m
@@ -15,6 +15,14 @@ const CFStringRef kUTTypeXMLPropertyList = CFSTR("com.apple.xml-property-list");
 const CFStringRef kUTTypeZipArchive = CFSTR("public.zip-archive");
 const CFStringRef kUTTypeAppleICNS = CFSTR("com.apple.icns");
 const CFStringRef kUTTypeFileURL = CFSTR("public.file-url");
+const CFStringRef kUTTypeCHeader = CFSTR("public.c-header");
+const CFStringRef kUTTypeCPlusPlusHeader = CFSTR("public.c-plus-plus-header");
+const CFStringRef kUTTypeCPlusPlusSource = CFSTR("public.c-plus-plus-source");
+const CFStringRef kUTTypeCSource = CFSTR("public.c-source");
+const CFStringRef kUTTypeFolder = CFSTR("public.folder");
+const CFStringRef kUTTypeObjectiveCPlusPlusSource = CFSTR("public.objective-c-plus-plus-source");
+const CFStringRef kUTTypeObjectiveCSource = CFSTR("public.objective-c-source");
+const CFStringRef kUTTypeTagSpecificationKey = CFSTR("UTTypeTagSpecification");
 
 const CFStringRef _kLSASNKey = CFSTR("LSASN");
 const CFStringRef _kLSApplicationTypeKey = CFSTR("ApplicationType");
