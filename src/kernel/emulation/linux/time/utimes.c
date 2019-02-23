c7322606b01e3f5a6cea8f792199a226accdcdd3
Sergey Bugaev <bugaevc@gmail.com>
2019-02-23 15:05:30 +0300

    Fix sys_utimes(path, NULL) crashing

--- src/kernel/emulation/linux/time/utimes.c
+++ src/kernel/emulation/linux/time/utimes.c
@@ -8,12 +8,14 @@ long sys_utimes(const char* path, struct bsd_timeval* tv)
 	int ret;
 	struct linux_timeval ltv[2];
 
-	ltv[0].tv_sec = tv[0].tv_sec;
-	ltv[0].tv_usec = tv[0].tv_usec;
-	ltv[1].tv_sec = tv[1].tv_sec;
-	ltv[1].tv_usec = tv[1].tv_usec;
+	if (tv) {
+		ltv[0].tv_sec = tv[0].tv_sec;
+		ltv[0].tv_usec = tv[0].tv_usec;
+		ltv[1].tv_sec = tv[1].tv_sec;
+		ltv[1].tv_usec = tv[1].tv_usec;
+	}
 
-	ret = LINUX_SYSCALL(__NR_utimes, path, ltv);
+	ret = LINUX_SYSCALL(__NR_utimes, path, tv ? ltv : 0);
 	if (ret < 0)
 		ret = errno_linux_to_bsd(ret);
 
