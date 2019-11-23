48b099771b9ccdfa783091307a2b86a8fffaf813
Andrew Hyatt <ahyattdev@icloud.com>
2019-11-23 17:30:06 -0500

    Shut down running instances during install
    This prevents forgetting to shut down darling and wondering why
    your changes have no effect.

---
+++ tools/shutdown-user.sh
@@ -0,0 +1,15 @@
+echo "Seeing if Darling is currently running"
+
+PID=`pgrep launchd`
+
+while [[ ! -z $PID ]]
+do
+	THISPID=$(echo $PID | head -n1)
+	RUNNING_USER=$(ps -o uname= -p $THISPID)
+	echo "Darling currently running for $RUNNING_USER, shutting it down..."
+	sudo -u $RUNNING_USER darling shutdown
+	sleep 2
+	PID=`pgrep launchd`
+done
+
+echo "No instances running now"
