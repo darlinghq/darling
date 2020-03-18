30b3b84c5266e0546a4f8a8052b7c36fd77aa393
Yingsheng Tian <58492046+Yingsheng-eroad@users.noreply.github.com>
2020-03-19 02:54:41 +1300

    add AVKit and MultipeerConnectivity framework (#712)
    * add 3 frameworks: AVKit MultipeerConnectivity CoreGraphics
    
    * add CoreGraphics
    
    * remove new CoreGraphics
    
    * move AVKit MultipeerConnectivity to frameworks
    
    * move AVKit MultipeerConnectivity to frameworks
    
    * move AVKit MultipeerConnectivity to frameworks

---
+++ src/frameworks/MultipeerConnectivity/include/MultipeerConnectivity/MultipeerConnectivity.h
@@ -0,0 +1,54 @@
+/*
+ This file is part of Darling.
+
+ Copyright (C) 2019 Lubos Dolezel
+
+ Darling is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Darling is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Darling.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef _MultipeerConnectivity_H_
+#define _MultipeerConnectivity_H_
+
+#import <Foundation/Foundation.h>
+
+#import <MultipeerConnectivity/MCNearbyServiceAdvertiserDelegate.h>
+#import <MultipeerConnectivity/MCNearbyServiceBrowserDelegate.h>
+#import <MultipeerConnectivity/MCSessionPrivateDelegate.h>
+#import <MultipeerConnectivity/NSNetServiceBrowserDelegate.h>
+#import <MultipeerConnectivity/NSNetServiceDelegate.h>
+#import <MultipeerConnectivity/NSStreamDelegate.h>
+#import <MultipeerConnectivity/NSURLSessionDataDelegate.h>
+#import <MultipeerConnectivity/NSURLSessionDelegate.h>
+#import <MultipeerConnectivity/NSURLSessionTaskDelegate.h>
+#import <MultipeerConnectivity/MCBrowserViewController.h>
+#import <MultipeerConnectivity/MCAdvertiserAssistant.h>
+#import <MultipeerConnectivity/MCSessionPeerState.h>
+#import <MultipeerConnectivity/MCSessionStream.h>
+#import <MultipeerConnectivity/MCResourceDownloader.h>
+#import <MultipeerConnectivity/MCSessionPeerConnectionData.h>
+#import <MultipeerConnectivity/MCResourceProgressObserver.h>
+#import <MultipeerConnectivity/MCSession.h>
+#import <MultipeerConnectivity/MCNearbyServiceUtils.h>
+#import <MultipeerConnectivity/MCNearbyDiscoveryPeerConnection.h>
+#import <MultipeerConnectivity/AWDMultipeerSessionMaxPeersInfo.h>
+#import <MultipeerConnectivity/MCNearbyServiceAdvertiser.h>
+#import <MultipeerConnectivity/MCNearbyServiceBrowser.h>
+#import <MultipeerConnectivity/MCNearbyDiscoveryPeer.h>
+#import <MultipeerConnectivity/AWDMultipeerSessionPeerConnectivityInfo.h>
+#import <MultipeerConnectivity/MCPeerIDInternal.h>
+#import <MultipeerConnectivity/MCPeerID.h>
+
+
+#endif
