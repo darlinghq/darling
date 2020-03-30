45db3a9c6c9a2fbfee2490725deac0f609cc4c69
Lubos Dolezel <lubos@dolezel.info>
2020-03-30 16:01:13 +0200

    CoreServices: CoreEndian.h -> Endian.h

--- src/frameworks/CoreServices/CoreEndian.cpp
+++ src/frameworks/CoreServices/CoreEndian.cpp
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with Darling.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "CoreEndian.h"
+#include <CoreServices/Endian.h>
 #include <stdint.h>
 #include <map>
 #include <TargetConditionals.h>
