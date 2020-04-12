900d4ff5ea6fdeb7163eb8dd5f547b1895a5ae27
Andrew Hyatt <ahyattdev@icloud.com>
2020-04-12 17:40:17 -0400

    Support the -l option with sudo (#357)

--- src/tools/sudo.c
+++ src/tools/sudo.c
@@ -17,67 +17,125 @@
  along with Darling.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
 
 unsigned int firstarg = 1, uid = 0, gid = 0;
+
+static int list_mode = 0;
 
 int argparse(int, char**);
 int usage(int, int);
 
 int main(int argc, char **argv)
 {
-	if ( ( argparse(argc, argv) || usage(argc, 0) ) == 1 )
-		return 1;
+	char *path = getenv("PATH"), *path_orig, *fullpath = NULL;
+	int i, len;
+	if (argparse(argc, argv))
+			return 1;
 
 	setuid(uid);
 	setgid(gid);
 
-	execvp(argv[firstarg], &argv[firstarg]);
-	perror("Error");
-	return 1;
+	if (firstarg == argc && list_mode)
+	{
+		printf("List mode without command not supported yet.\n");
+		return EXIT_SUCCESS;
+	}
+	else if (list_mode)
+	{
+		/* Print fill path to command, then args if present */
+		if (!path)
+			printf("can't search for command if no path\n");
+		path = strdup(path);
+		path_orig = path;
+		len = strlen(path_orig);
+		for (i = 0; i < len; i++)
+		{
+			if (path_orig[i] == ':' || path_orig[i] == '\0')
+			{
+				path_orig[i] = '\0';
+				fullpath = malloc(strlen(argv[firstarg])+strlen(path)+2);
+				strcpy(fullpath, path);
+				fullpath[strlen(path)] = '/';
+				strcpy(fullpath+strlen(path)+1, argv[firstarg]);
+				if (access(fullpath, X_OK) == 0)
+					break;
+				free(fullpath);
+				fullpath = NULL;
+				path = path_orig+i+1;
+			}
+		}
+		free(path_orig);
+		if (!fullpath)
+		{
+			fprintf(stderr, "%s: %s: command not found\n", argv[0], argv[firstarg]);
+			return EXIT_FAILURE;
+		}
+		printf("%s", fullpath);
+		free(fullpath);
+		for (i = firstarg+1; i < argc; i++)
+		{
+			printf(" %s", argv[i]);
+		}
+		putchar('\n');
+		return EXIT_SUCCESS;
+	}
+	else
+	{
+		if (firstarg < argc)
+		{
+			execvp(argv[firstarg], &argv[firstarg]);
+			perror("Running sudo failed");
+		}
+		else
+			return EXIT_SUCCESS;
+	}
+	return EXIT_FAILURE;
 }
 
 int argparse(int argc, char **argv)
 {
-	while( (argv[firstarg] != NULL) && (!strncmp(argv[firstarg], "-", 1)) )
+	for (firstarg = 1; firstarg < argc; firstarg++)
 	{
 		if(!strcmp(argv[firstarg], "-g"))
 		{
 			sscanf(argv[++firstarg], "%u", &gid);
-			firstarg++;
 		}
 		else if(!strcmp(argv[firstarg], "-u"))
 		{
 			sscanf(argv[++firstarg], "%u", &uid);
-			firstarg++;
 		}
 		else if (!strcmp(argv[firstarg], "-A"))
 		{
-			firstarg++;
 		}
 		else if (!strcmp(argv[firstarg], "-n"))
 		{
-			firstarg++;
 		}
 		else if (!strcmp(argv[firstarg], "-v"))
 		{
-			firstarg++;
 		}
 		else if(!strcmp(argv[firstarg], "--"))
 		{
-			firstarg++;
 			break;
 		}
 		else if(!strcmp(argv[firstarg], "-k"))
-			firstarg++;
+		{
+		}
 		else if(!strcmp(argv[firstarg], "--help"))
+		{
 			return usage(argc, 1);
+		}
+		else if (!strcmp(argv[firstarg], "-l"))
+		{
+			list_mode = 1;
+		}
 		else
 		{
-			fprintf(stderr, "Unknown option: %s\n", argv[firstarg]);
-			return usage(argc, 1);
+			break;
 		}
 	}
 	return 0;
@@ -85,7 +143,7 @@ int argparse(int argc, char **argv)
 
 int usage(int argc, int arg_help)
 {
-	if (argc <= (int)firstarg || arg_help == 1)
+	if (argc <= firstarg || arg_help == 1)
 	{
 		fprintf(stderr, "This is a fake 'sudo' implementation, intended for use in Darling.\n"
 				"Processes will think they are run as UID/GID 0, but they are still run as your original UID/GID.\n"
