18751d66acb62c890968a95acae6442ac081991c
Balint66 <44706571+Balint66@users.noreply.github.com>
2020-04-20 20:27:28 +0200

    Adding max_basic

--- src/kernel/emulation/linux/misc/sysctl_machdep.c
+++ src/kernel/emulation/linux/misc/sysctl_machdep.c
@@ -7,12 +7,15 @@ enum {
 
 enum {
 	_CPU_BRAND_STRING = 1000,
+	_CPU_MAX_BASIC=999,
 };
 
 static sysctl_handler(handle_brand_string);
+static sysctl_handler(handle_max_basic);
 
 const struct known_sysctl sysctls_machdep_cpu[] = {
 	{ .oid = _CPU_BRAND_STRING, .type = CTLTYPE_STRING, .exttype = "S", .name = "brand_string", .handler = handle_brand_string },
+	{ .oid = _CPU_MAX_BASIC, .type = CTLTYPE_INT, .exttype = "I", .name = "max_basic", .handler = handle_max_basic },
 	{ .oid = -1 }
 };
 
@@ -47,6 +50,29 @@ sysctl_handler(handle_brand_string)
 
 	v.name[12] = 0;
 	copyout_string(v.name, (char*) old, oldlen);
+
+	return 0;
+}
+
+sysctl_handler(handle_max_basic)
+{
+	unsigned int level = 0;
+    unsigned int eax = 0;
+	union
+	{
+		struct
+		{
+	    	unsigned int ebx;
+		    unsigned int edx;
+	    	unsigned int ecx;
+		};
+		char name[13];
+	} v;
+
+    __cpuid(level, eax, v.ebx, v.ecx, v.edx);
+
+	
+	sprintf(old,"%d",eax);
 
 	return 0;
 }
