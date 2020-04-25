8558ae846cc7b430a899733a222ddcf75854bd20
Ariel Abreu <facekapow@outlook.com>
2020-04-25 10:10:26 -0400

    Use `llistxattr` for symlinks in `flistxattr`
    The last commit changed `openat` to use Linux O_PATH for BSD O_SYMLINK, which is necessary to open a file handle to the symlink itself rather than its target. Unfortunately, this means that some syscalls (like `flistxattr`) need to modified because Linux doesn't support O_PATH descriptors in them
    
    `fgetxattr` and `fsetxattr` will also need fixing

--- src/kernel/emulation/linux/xattr/flistxattr.c
+++ src/kernel/emulation/linux/xattr/flistxattr.c
@@ -2,12 +2,43 @@
 #include "../base.h"
 #include "../errno.h"
 #include <linux-syscalls/linux.h>
+#include "../stat/common.h"
+#include "../fdpath.h"
+#include <sys/stat.h>
+#include "../simple.h"
+
+#ifdef __NR_fstat64
+	#define STAT_CALL __NR_fstat64
+#else
+	#define STAT_CALL __NR_fstat
+#endif
+#ifndef MAXPATHLEN
+#	define MAXPATHLEN 1024
+#endif
 
 long sys_flistxattr(int fd, char* namebuf, unsigned long size, int options)
 {
 	int ret;
 
-	ret = LINUX_SYSCALL(__NR_flistxattr, fd, namebuf, size);
+	struct linux_stat st;
+	ret = LINUX_SYSCALL(STAT_CALL, fd, &st);
+	if (ret < 0)
+		return errno_linux_to_bsd(ret);
+
+	if (S_ISLNK(st.st_mode)) {
+		// for links, we need to use `llistxattr`
+		// so translate the fd to a Linux path
+		char buf[64] = {0};
+		char path[4096] = {0};
+		__simple_sprintf(buf, "/proc/self/fd/%d", fd);
+		ret = LINUX_SYSCALL(__NR_readlink, buf, path, sizeof(path) - 1);
+		if (ret < 0)
+			return errno_linux_to_bsd(ret);
+		path[ret] = '\0';
+		ret = LINUX_SYSCALL(__NR_llistxattr, path, namebuf, size);
+	} else {
+		ret = LINUX_SYSCALL(__NR_flistxattr, fd, namebuf, size);
+	}
 
 	if (ret < 0)
 		return errno_linux_to_bsd(ret);
