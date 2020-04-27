de3aa420ce142f36f91a9778c05261284e2e2d11
Lubos Dolezel <lubos@dolezel.info>
2020-04-27 11:34:56 +0200

    Handle dup/dup2 on kqueue objects

--- src/kernel/emulation/linux/unistd/dup2.c
+++ src/kernel/emulation/linux/unistd/dup2.c
@@ -3,6 +3,8 @@
 #include "../errno.h"
 #include <linux-syscalls/linux.h>
 
+extern void kqueue_dup(int oldfd, int newfd);
+
 long sys_dup2(int fd_from, int fd_to)
 {
 	int ret;
@@ -10,6 +12,8 @@ long sys_dup2(int fd_from, int fd_to)
 	ret = LINUX_SYSCALL2(__NR_dup2, fd_from, fd_to);
 	if (ret < 0)
 		ret = errno_linux_to_bsd(ret);
+	else
+		kqueue_dup(fd_from, fd_to);
 
 	return ret;
 }
