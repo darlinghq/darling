4ccaf36d8d315c89e49b4092eebdc3e8362bf967
Hin-Tak Leung <htl10@users.sourceforge.net>
2020-06-08 09:02:54 +0100

    moving constants from LaunchServices into the same group, for easier look-up

--- src/frameworks/CoreServices/constants.m
+++ src/frameworks/CoreServices/constants.m
@@ -1,6 +1,5 @@
 #include <CoreFoundation/CoreFoundation.h>
 
-const CFStringRef _kLSPIDKey = CFSTR("pid");
 const CFStringRef kUTTagClassDeviceModelCode = CFSTR("com.apple.device-model-code");
 const CFStringRef kUTTagClassFilenameExtension = CFSTR("public.filename-extension");
 const CFStringRef kUTTagClassMIMEType = CFSTR("public.mime-type");
@@ -37,6 +36,8 @@ const CFStringRef kUTTypePICT = CFSTR("com.apple.pict");
 const CFStringRef kUTTypeUTF16ExternalPlainText = CFSTR("public.utf16-external-plain-text");
 const CFStringRef kUTTypeUTF16PlainText = CFSTR("public.utf16-plain-text");
 
+/* re-exported from LaunchServices */
+const CFStringRef _kLSPIDKey = CFSTR("pid");
 const CFStringRef _kLSASNKey = CFSTR("LSASN");
 const CFStringRef _kLSApplicationTypeKey = CFSTR("ApplicationType");
 const CFStringRef _kLSApplicationUIElementTypeKey = CFSTR("UIElement");
