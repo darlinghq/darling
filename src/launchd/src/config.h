c70e4fe558710901f00ef497192125c00cf53679
Ariel Abreu <facekapow@outlook.com>
2020-07-01 22:43:37 -0400

    Modify launchd to build for Darling

--- src/launchd/src/config.h
+++ src/launchd/src/config.h
@@ -3,27 +3,27 @@
 
 #include <TargetConditionals.h>
 
-#if __has_include(<quarantine.h>)
+#if __has_include(<quarantine.h>) && !defined(DARLING)
 #define HAVE_QUARANTINE 1
 #else
 #define HAVE_QUARANTINE 0
 #endif
 
-#if __has_include(<responsibility.h>)
+#if __has_include(<responsibility.h>) && !defined(DARLING)
 #define HAVE_RESPONSIBILITY 1
 #else
 #define HAVE_RESPONSIBILITY 0
 #endif
 
-#if __has_include(<sandbox.h>)
+#if __has_include(<sandbox.h>) && !defined(DARLING)
 #define HAVE_SANDBOX 1
 #else
 #define HAVE_SANDBOX 0
 #endif
 
-#define HAVE_LIBAUDITD !TARGET_OS_EMBEDDED
+#define HAVE_LIBAUDITD !TARGET_OS_EMBEDDED && !defined(DARLING)
 
-#if !TARGET_OS_EMBEDDED && __has_include(<systemstats/systemstats.h>)
+#if !TARGET_OS_EMBEDDED && __has_include(<systemstats/systemstats.h>) && !defined(DARLING)
 #define HAVE_SYSTEMSTATS 1
 #else
 #define HAVE_SYSTEMSTATS 0
