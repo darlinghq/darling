4d64d364c8939aac5d5df8a95df7c971cccf051c
Ariel Abreu <facekapow@outlook.com>
2020-07-05 13:47:43 -0400

    Add some more declarations to os/state_private.h

--- src/kernel/include/os/state_private.h
+++ src/kernel/include/os/state_private.h
@@ -8,6 +8,9 @@
 
 #define OS_STATE_DATA_SERIALIZED_NSCF_OBJECT 1
 
+#define OS_STATE_API_FAULT 0
+#define OS_STATE_API_REQUEST 1
+
 #define OS_STATE_DATA_SIZE_NEEDED(statelen) statelen
 
 typedef struct {
@@ -15,6 +18,7 @@ typedef struct {
 	int osd_size;
 	char *osd_title;
 	void *osd_data;
+	int osd_data_size;
 } *os_state_data_t;
 
 typedef struct {
@@ -23,6 +27,11 @@ typedef struct {
 
 typedef os_state_data_t(^os_state_block_t)(os_state_hints_t hints);
 
-void os_state_add_handler(dispatch_queue_t arg0, os_state_block_t arg1);
+// not sure what the real type for this is
+typedef int os_state_handle_t;
+
+os_state_handle_t os_state_add_handler(dispatch_queue_t arg0, os_state_block_t arg1);
+
+void os_state_remove_handler(os_state_handle_t handler);
 
 #endif
