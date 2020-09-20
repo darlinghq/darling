5af5695758a496a4611caa8ff10fac34a1f7d76d
Ariel Abreu <facekapow@outlook.com>
2020-09-20 18:42:07 -0400

    Allow "simple" libsystem_kernel functions to be used in C++

--- src/kernel/emulation/linux/simple.h
+++ src/kernel/emulation/linux/simple.h
@@ -1,6 +1,10 @@
 #ifndef LINUX_DEBUG_H
 #define LINUX_DEBUG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void __simple_printf(const char* format, ...);
 void __simple_kprintf(const char* format, ...);
 void __simple_fprintf(int fd, const char* format, ...);
@@ -18,6 +22,10 @@ struct simple_readline_buf
 };
 void __simple_readline_init(struct simple_readline_buf* buf);
 char* __simple_readline(int fd, struct simple_readline_buf* buf, char* out, int max_out);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif
 
