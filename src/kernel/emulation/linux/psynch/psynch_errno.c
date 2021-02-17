e053be4e0669886d3bf06004986d10ae80e92182
Ariel Abreu <facekapow@outlook.com>
2021-02-16 23:29:31 -0500

    Support the use of the new kernel psynch code
    ...but don't enable it. It's broken at the moment.

--- src/kernel/emulation/linux/psynch/psynch_errno.c
+++ src/kernel/emulation/linux/psynch/psynch_errno.c
@@ -1,5 +1,9 @@
 #include "../errno.h"
 
+#ifndef USE_NEW_PSYNC_CODE
+	#define USE_NEW_PSYNC_CODE 0
+#endif
+
 // psynch syscalls send back extra bits and leave only the bottom 8 bits for errors
 // So we have to filter out the top 24 bits, translate errno and put the 24 bits back.
 int psynch_errno(int rv)
@@ -10,7 +14,11 @@ int psynch_errno(int rv)
 
 	rv = -rv;
 	top = rv & 0xffffff00;
+#if USE_NEW_PSYNC_CODE
+	rv = rv & 0xff;
+#else
 	rv = errno_linux_to_bsd(rv & 0xff);
+#endif
 	rv |= top;
 
 	return -rv;
