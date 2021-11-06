d974b14b63600bebd31264a431478685a03ac361
Thomas A <curioustommy@protonmail.com>
2021-11-06 07:18:45 -0700

    Stub `GetCompilerInclude` if not i386 and x86_64.
    cpuid.h only supports i386 and x86_64.

--- cmake/compiler_include.cmake
+++ cmake/compiler_include.cmake
@@ -1,32 +1,34 @@
 # Detect the location of compiler-specific header files
 # such as stdbool.h or xmmintrin.h
 function(GetCompilerInclude OutputVar)
-	file(WRITE "${CMAKE_BINARY_DIR}/cinctest.c" "#include <cpuid.h>")
-	execute_process(COMMAND "${CMAKE_C_COMPILER}" -M "${CMAKE_BINARY_DIR}/cinctest.c"
-		RESULT_VARIABLE BuildResult
-		OUTPUT_VARIABLE BuildOutput
-	)
+	if (TARGET_i386 OR TARGET_x86_64)
+		file(WRITE "${CMAKE_BINARY_DIR}/cinctest.c" "#include <cpuid.h>")
+		execute_process(COMMAND "${CMAKE_C_COMPILER}" -M "${CMAKE_BINARY_DIR}/cinctest.c"
+			RESULT_VARIABLE BuildResult
+			OUTPUT_VARIABLE BuildOutput
+		)
 
-	if (BuildResult)
-		message(FATAL_ERROR "Cannot detect compiler header include path")
-	endif (BuildResult)
+		if (BuildResult)
+			message(FATAL_ERROR "Cannot detect compiler header include path")
+		endif (BuildResult)
 
-	separate_arguments(BuildOutput)
+		separate_arguments(BuildOutput)
 
-	foreach (str ${BuildOutput})
-		string(REGEX REPLACE "\n$" "" str "${str}")
-		# message(STATUS "Output: ${str}")
+		foreach (str ${BuildOutput})
+			string(REGEX REPLACE "\n$" "" str "${str}")
+			# message(STATUS "Output: ${str}")
 
-		if (str MATCHES "cpuid.h$")
-			# message(STATUS "Str matched: ${str}")
-			string(REGEX REPLACE "cpuid\\.h" "" IncPath "${str}")
-		endif (str MATCHES "cpuid.h$")
-	endforeach (str)
+			if (str MATCHES "cpuid.h$")
+				# message(STATUS "Str matched: ${str}")
+				string(REGEX REPLACE "cpuid\\.h" "" IncPath "${str}")
+			endif (str MATCHES "cpuid.h$")
+		endforeach (str)
 
-	if (NOT IncPath)
-		message(FATAL_ERROR "Cannot parse compiler output to detect include path")
-	endif (NOT IncPath)
+		if (NOT IncPath)
+			message(FATAL_ERROR "Cannot parse compiler output to detect include path")
+		endif (NOT IncPath)
 
-	set(${OutputVar} ${IncPath} PARENT_SCOPE)
+		set(${OutputVar} ${IncPath} PARENT_SCOPE)
+	endif (TARGET_i386 OR TARGET_x86_64)
 endfunction(GetCompilerInclude)
 
