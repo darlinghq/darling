b19a2138d1f12cd01e04c2aacb877f156364feac
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2021-11-15 09:02:03 +0700

    fix(waitid): Proper marshalling for siginfo_t
    Linux and BSD siginfo_t has different layouts, making processes calling waitid confused about the output.

--- src/kernel/emulation/linux/process/waitid.c
+++ src/kernel/emulation/linux/process/waitid.c
@@ -6,22 +6,37 @@
 #include <linux-syscalls/linux.h>
 #include <stddef.h>
 
-long sys_waitid(int idtype, int id, struct simple_siginfo* si, int options)
+static void siginfo_linux_to_bsd(const linux_siginfo_t* linux_si, siginfo_t* si);
+
+long sys_waitid(int idtype, int id, siginfo_t* si, int options)
 {
 	int ret, linux_options;
 
 	linux_options = waitopts_bsd_to_linux(options);
 
-	ret = LINUX_SYSCALL(__NR_waitid, idtype, id, si, linux_options);
+	linux_siginfo_t linux_si;
+	ret = LINUX_SYSCALL(__NR_waitid, idtype, id, &linux_si, linux_options);
 	if (ret < 0)
 		return errno_linux_to_bsd(ret);
 
 	if (si != NULL)
 	{
-		si->si_signo = signum_linux_to_bsd(si->si_signo);
-		si->si_errno = errno_linux_to_bsd(si->si_errno);
+		siginfo_linux_to_bsd(&linux_si, si);
 	}
 
 	return ret;
+}
+
+static void siginfo_linux_to_bsd(const linux_siginfo_t* linux_si, siginfo_t* si)
+{
+	si->si_signo = signum_linux_to_bsd(linux_si->si_signo);
+	si->si_errno = errno_linux_to_bsd(linux_si->si_errno);
+	si->si_code = linux_si->si_code;
+	si->si_pid = linux_si->linux_si_pid;
+	si->si_uid = linux_si->linux_si_uid;
+	si->si_status = linux_si->linux_si_status;
+	si->si_addr = linux_si->linux_si_addr;
+	si->si_value = linux_si->linux_si_value;
+	si->si_band = linux_si->linux_si_band;
 }
 
