c19a0bb1b576bd634992cca0f9c9839efa312e35
Ariel Abreu <facekapow@outlook.com>
2022-02-12 00:45:33 -0500

    Implement EVFILT_PROC with darlingserver

--- src/kernel/emulation/linux/ext/for-libkqueue.c
+++ src/kernel/emulation/linux/ext/for-libkqueue.c
@@ -4,3 +4,7 @@
 int _dserver_rpc_kqchan_mach_port_open_4libkqueue(uint32_t port_name, void* receive_buffer, uint64_t receive_buffer_size, uint64_t saved_filter_flags, int* out_socket) {
 	return dserver_rpc_kqchan_mach_port_open(port_name, receive_buffer, receive_buffer_size, saved_filter_flags, out_socket);
 };
+
+int _dserver_rpc_kqchan_proc_open_4libkqueue(int32_t pid, uint32_t flags, int* out_socket) {
+	return dserver_rpc_kqchan_proc_open(pid, flags, out_socket);
+};
--- src/kernel/emulation/linux/ext/for-libkqueue.h
+++ src/kernel/emulation/linux/ext/for-libkqueue.h
@@ -8,4 +8,7 @@
 VISIBLE
 int _dserver_rpc_kqchan_mach_port_open_4libkqueue(uint32_t port_name, void* receive_buffer, uint64_t receive_buffer_size, uint64_t saved_filter_flags, int* out_socket);
 
+VISIBLE
+int _dserver_rpc_kqchan_proc_open_4libkqueue(int32_t pid, uint32_t flags, int* out_socket);
+
 #endif // _DARLING_EMULATION_FOR_LIBKQUEUE_H_
