3b17cb5472e05abadc4a35a00f5104dcc7c99130
Marco Rolappe <m_rolappe@gmx.net>
2022-03-24 00:35:36 +0100

    SwiftUI framework stubs

---
+++ src/frameworks/SwiftUI/include/SwiftUI/_TtCV7SwiftUI13ViewTransformP33_CE19A3CEA6B9730579C42CE4C3071E745Chunk.h
@@ -0,0 +1,24 @@
+/*
+ This file is part of Darling.
+
+ Copyright (C) 2019 Lubos Dolezel
+
+ Darling is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Darling is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Darling.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Foundation/Foundation.h>
+
+@interface _TtCV7SwiftUI13ViewTransformP33_CE19A3CEA6B9730579C42CE4C3071E745Chunk : NSObject
+
+@end
