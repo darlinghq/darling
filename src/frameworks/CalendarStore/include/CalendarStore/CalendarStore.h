8b434dda9a37b559d06a21eedabd5c51bf549922
Marco Rolappe <m_rolappe@gmx.net>
2022-03-23 21:09:14 +0100

    CalendarStore framework stubs

---
+++ src/frameworks/CalendarStore/include/CalendarStore/CalendarStore.h
@@ -0,0 +1,44 @@
+/*
+ This file is part of Darling.
+
+ Copyright (C) 2019 Lubos Dolezel
+
+ Darling is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Darling is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Darling.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef _CalendarStore_H_
+#define _CalendarStore_H_
+
+#import <Foundation/Foundation.h>
+
+#import <CalendarStore/CalendarAgentClient.h>
+#import <CalendarStore/CalPredicate.h>
+#import <CalendarStore/CalEventPredicate.h>
+#import <CalendarStore/CalAttendee.h>
+#import <CalendarStore/CalGroup.h>
+#import <CalendarStore/CalTaskPredicate.h>
+#import <CalendarStore/CalCalendarStore.h>
+#import <CalendarStore/CalCalendar.h>
+#import <CalendarStore/CalAlarm.h>
+#import <CalendarStore/CalEvent.h>
+#import <CalendarStore/CalRecurrenceRule.h>
+#import <CalendarStore/CalNthWeekDay.h>
+#import <CalendarStore/CalRecurrenceEnd.h>
+#import <CalendarStore/CalCalendarItemPredicate.h>
+#import <CalendarStore/CalCalendarItem.h>
+#import <CalendarStore/CalTask.h>
+
+
+#endif
