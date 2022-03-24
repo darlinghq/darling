64f9c4ae066c837a3b973fecb6ab55c88a56c050
Marco Rolappe <m_rolappe@gmx.net>
2022-03-24 16:59:27 +0100

    CoreBluetooth framework stubs

---
+++ src/frameworks/CoreBluetooth/include/CoreBluetooth/CoreBluetooth.h
@@ -0,0 +1,54 @@
+/*
+ This file is part of Darling.
+
+ Copyright (C) 2019 Lubos Dolezel
+
+ Darling is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Darling is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Darling.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#ifndef _CoreBluetooth_H_
+#define _CoreBluetooth_H_
+
+#import <Foundation/Foundation.h>
+
+#import <CoreBluetooth/CBPairingAgentParentDelegate.h>
+#import <CoreBluetooth/CBXpcConnectionDelegate.h>
+#import <CoreBluetooth/BTDevicePicker.h>
+#import <CoreBluetooth/CBClassicPeer.h>
+#import <CoreBluetooth/CBCentralManager.h>
+#import <CoreBluetooth/CBRFCOMMChannel.h>
+#import <CoreBluetooth/CBCharacteristic.h>
+#import <CoreBluetooth/CBMutableCharacteristic.h>
+#import <CoreBluetooth/CBScalablePipeManager.h>
+#import <CoreBluetooth/CBAttribute.h>
+#import <CoreBluetooth/CBScalablePipe.h>
+#import <CoreBluetooth/CBDescriptor.h>
+#import <CoreBluetooth/CBMutableDescriptor.h>
+#import <CoreBluetooth/CBPeripheral.h>
+#import <CoreBluetooth/CBService.h>
+#import <CoreBluetooth/CBMutableService.h>
+#import <CoreBluetooth/CBUUID.h>
+#import <CoreBluetooth/CBL2CAPChannel.h>
+#import <CoreBluetooth/CBPeer.h>
+#import <CoreBluetooth/CBClassicManager.h>
+#import <CoreBluetooth/CBXpcConnection.h>
+#import <CoreBluetooth/CBPeripheralManager.h>
+#import <CoreBluetooth/CBCentral.h>
+#import <CoreBluetooth/CBManager.h>
+#import <CoreBluetooth/CBATTRequest.h>
+#import <CoreBluetooth/CBPairingAgent.h>
+
+
+#endif
