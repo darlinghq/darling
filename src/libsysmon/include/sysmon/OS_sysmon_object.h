0876832551c5ab94cabbc409f02d6586c0f4d3e3
Thomas A <curioustommy@protonmail.com>
2022-04-27 22:06:21 -0700

    [libsysmon] Remove OS_object from OS_sysmon_object.h
    There seems to be a duplicate conflict:
    
    ```
    /home/user/Downloads/darling/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/sysmon/OS_sysmon_object.h:20:1: error: duplicate interface definition for class 'OS_object'
    @interface OS_object
    ^
    /home/user/Downloads/darling/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include/os/object.h:203:1: note: previous definition is here
    OS_OBJECT_DECL_BASE(object, NSObject);
    ```

--- src/libsysmon/include/sysmon/OS_sysmon_object.h
+++ src/libsysmon/include/sysmon/OS_sysmon_object.h
@@ -17,9 +17,6 @@ You should have received a copy of the GNU General Public License
 along with Darling.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-@interface OS_object
-
-@end
 
 @interface OS_sysmon_object : OS_object
 
