37761a66fdb1324516d30a6219f9ab6762f68001
Thomas A <curioustommy@protonmail.com>
2022-05-20 22:14:13 -0700

    Generate bsdsyscalls using xnu-7195.141.2 files

---
+++ src/kernel/libsyscall/bsdsyscalls/_stat.S
@@ -0,0 +1,13 @@
+#define __SYSCALL_32BIT_ARG_BYTES 8
+#include "SYS.h"
+
+#ifndef SYS_stat
+#error "SYS_stat not defined. The header files libsyscall is building against do not match syscalls.master."
+#endif
+
+#if defined(__i386__) || defined(__x86_64__) || defined(__ppc__)
+__SYSCALL2(_stat, stat, 2, cerror)
+#else
+__SYSCALL2(___stat, stat, 2, cerror)
+#endif
+
