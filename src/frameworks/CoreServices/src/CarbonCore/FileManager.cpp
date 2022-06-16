ca7013969795c01e2d5634ecfb35f5a82d607475
Bastiaan Quast <bquast@gmail.com>
2022-06-16 19:41:15 +0200

    Update FileManager.cpp

--- src/frameworks/CoreServices/src/CarbonCore/FileManager.cpp
+++ src/frameworks/CoreServices/src/CarbonCore/FileManager.cpp
@@ -1,7 +1,7 @@
 /*
 This file is part of Darling.
 
-Copyright (C) 2012-2020 Lubos Dolezel
+Copyright (C) 2012-2022 Lubos Dolezel
 
 Darling is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
