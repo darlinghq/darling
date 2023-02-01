4cf5d2cf4f9a3b6c2de818c86a01e00174140add
Thomas A <curioustommy@protonmail.com>
2023-01-31 22:10:53 -0800

    Remove keymgr from Darling repo.

--- src/keymgr/testcases/3043647/exc.cc
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-extern "C" int do_test(int n)
-{
-  printf ("Entering do_test\n");
-  printf ("Normal return from do_test.\n");
-  return 0;
-}
--- src/keymgr/testcases/3074709/plugin2.cc
+++
@@ -1,7 +0,0 @@
-typedef void (*thrower)();
-
-extern "C" void
-do_call_thrower (thrower do_throw)
-{
-  do_throw();
-}
