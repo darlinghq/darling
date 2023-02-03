24243d1c82b8c3bb618561c0bb9dbfea9249dcb7
Thomas A <curioustommy@protonmail.com>
2023-02-02 20:12:28 -0800

    Remove libiconv From Darling Repo

--- src/libiconv/libiconv/include/export.h
+++
@@ -1,6 +0,0 @@
-
-#if @HAVE_VISIBILITY@ && BUILDING_LIBICONV
-#define __LIBICONV_DLL_EXPORTED __attribute__((__visibility__("default")))
-#else
-#define __LIBICONV_DLL_EXPORTED
-#endif
--- src/libiconv/libiconv/windows/dllexport.h
+++
@@ -1,6 +0,0 @@
-
-#ifdef BUILDING_LIBICONV
-#define __LIBICONV_DLL_EXPORTED __declspec(dllexport)
-#else
-#define __LIBICONV_DLL_EXPORTED __declspec(dllimport)
-#endif
