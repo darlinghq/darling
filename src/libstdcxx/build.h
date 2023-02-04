5e6ada53c39e395093d80f8270a3c26a4b86e7d3
Thomas A <curioustommy@protonmail.com>
2023-02-04 08:50:14 -0800

    Remove libstdcxx From Darling Repo

--- src/libstdcxx/build.h
+++
@@ -1,3 +0,0 @@
-#define _GLIBCXX_NOTHROW throw()
-#define __private_extern__ __attribute__((visibility("hidden")))
-
--- src/libstdcxx/src/dso_handle.c
+++
@@ -1,2 +0,0 @@
-const void *const __dso_handle __attribute__ ((__visibility__ ("hidden"))) = &__dso_handle;
-
