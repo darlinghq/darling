1655ecefbf7c38d03d3e9f27bca4dd6d270a6a73
Daniel Gretarsson <daniel@genkiinstruments.com>
2023-03-17 11:08:38 +0000

    Add definitions for missing constants in CoreMIDI

---
+++ src/frameworks/CoreMIDI/src/MIDIServices.m
@@ -0,0 +1,5 @@
+#import <CoreMIDI/MIDIServices.h>
+
+const CFStringRef kMIDIPropertyName = nil;
+const CFStringRef kMIDIPropertyConnectionUniqueID = nil;
+const CFStringRef kMIDIPropertyUniqueID = nil;
