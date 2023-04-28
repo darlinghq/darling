20b1b34fba9c2e5d53a81027b26c7cbe9cd7b0bd
Christian <57967583+CKegel@users.noreply.github.com>
2023-04-28 13:55:49 -0400

    Add AE TypeDefs for AppKit Apps (#1363)
    added typedefs for Appkit Events

--- src/frameworks/CoreServices/include/AE/AE.h
+++ src/frameworks/CoreServices/include/AE/AE.h
@@ -2,6 +2,7 @@
 #define __AE_H__
 
 #include <AE/AEDataModel.h>
+#include <AE/AppleEvents.h>
 
 OSErr AECreateDesc(DescType typeCode, const void *dataPtr, Size dataSize, AEDesc *result);
 OSErr AEDisposeDesc(AEDesc *theAEDesc);
