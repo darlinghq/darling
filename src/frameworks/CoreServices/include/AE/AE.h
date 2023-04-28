c4e74c3fd458562cb351165c75189f7c8779a8cf
ckegel <57967583+CKegel@users.noreply.github.com>
2023-04-28 13:35:21 -0400

    remove duplicated declarations

--- src/frameworks/CoreServices/include/AE/AE.h
+++ src/frameworks/CoreServices/include/AE/AE.h
@@ -2,24 +2,9 @@
 #define __AE_H__
 
 #include <AE/AEDataModel.h>
+#include <AE/AppleEvents.h>
 
 OSErr AECreateDesc(DescType typeCode, const void *dataPtr, Size dataSize, AEDesc *result);
 OSErr AEDisposeDesc(AEDesc *theAEDesc);
-
-enum : AEEventID {
-kAEOpenApplication = 'oapp',
-kAEReopenApplication = 'rapp',
-kAEOpenDocuments = 'odoc',
-kAEPrintDocuments = 'pdoc',
-kAEOpenContents = 'ocon',
-kAEQuitApplication = 'quit',
-kAEAnswer = 'ansr',
-kAEApplicationDied = 'obit',
-kAEShowPreferences = 'pref',
-};
-
-enum : DescType {
-    kCoreEventClass = 'aevt',
-};
 
 #endif
