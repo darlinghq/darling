6ab07320ef644433c7e11dada0cbe89f271f2c9e
Thomas A. <curioustommy@protonmail.com>
2024-08-31 10:54:56 -0700

    Add Note About Some Of These Values Intentionally Being "nil"
    Usually, we want to avoid lazily setting values as "nil". However, there are some instances where Apple would set these values as "nil".

--- src/frameworks/CoreMIDI/src/MIDIServices.m
+++ src/frameworks/CoreMIDI/src/MIDIServices.m
@@ -1,5 +1,9 @@
 #import <CoreMIDI/MIDIServices.h>
 
+// Note: It is expected for some of these values to be assigned `nil`,
+// Not sure why Apple doesn't actually assign a CFString value to these 
+// variables...
+
 /* Identification */
 const CFStringRef kMIDIPropertyName = nil;
 const CFStringRef kMIDIPropertyUniqueID = nil;
